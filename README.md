454b9885b78574c84059f248c778dd0f025829c9
Hamdi NAIT LIMAM <hnaitlimam@vermeg.com>
2022-06-01 09:40:12 +0100

    init 7 projects

---
+++ README.md
@@ -0,0 +1,8 @@
+BookStore project (Microservice Version) developed by : 
+
+-> Hamdi Nait Limam <br />
+-> Baha Sahli <br />
+-> Nourhene Abbes  <br />
+-> Maha Bacha  <br />
+-> Malek Kouissi  <br />
+-> Hamza Bouchareb
---
+++ book-service/src/test/java/tn/esprit/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
