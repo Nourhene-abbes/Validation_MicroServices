00d71bf713c3a05fa725c57e02bb463286e5b765
Hamdi NAIT LIMAM <hnaitlimam@vermeg.com>
2022-06-01 18:22:08 +0100

    modidied test files

--- basket-service/src/main/java/tn/esprit/basketservice/model/Book.java
+++ basket-service/src/main/java/tn/esprit/basketservice/model/Book.java
@@ -1,4 +1,4 @@
-package tn.esprit.wishlistservice.model;
+package tn.esprit.basketservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
--- basket-service/src/main/java/tn/esprit/basketservice/model/FavoriteCategorie.java
+++ basket-service/src/main/java/tn/esprit/basketservice/model/FavoriteCategorie.java
@@ -1,4 +1,4 @@
-package tn.esprit.wishlistservice.model;
+package tn.esprit.basketservice.model;
 
 import java.io.Serializable;
 
