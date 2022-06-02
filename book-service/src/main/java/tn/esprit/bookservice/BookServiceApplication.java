90e58232397b7dac82c26160fdff236babb89a0d
Hamdi NAIT LIMAM <hnaitlimam@vermeg.com>
2022-06-02 23:45:15 +0100

    fixed main class

--- book-service/src/main/java/tn/esprit/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/tn/esprit/bookservice/BookServiceApplication.java
@@ -1,8 +1,6 @@
 package tn.esprit.bookservice;
 
-import java.util.Date;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,21 +8,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.client.RestTemplate;
 
-import tn.esprit.bookservice.controller.BookController;
-import tn.esprit.bookservice.model.Book;
-
 @SpringBootApplication
 @EnableEurekaClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
-	}
-	
-	@Bean(name="entityManagerFactory")
-	public LocalSessionFactoryBean sessionFactory() {
-	LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-	return sessionFactory;
 	}
 	
 	@Bean
